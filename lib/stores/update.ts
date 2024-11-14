e162c9d4b25eb6681fac2043f1c7a3400b7f8bd3
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-14 15:03:51 +0900

    delete devtools

--- lib/stores/update.ts
+++ lib/stores/update.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
 
 interface UpdateState {
   state: string;
@@ -9,9 +8,7 @@ interface UpdateAction {
   setUpdate: (state: string) => void;
 }
 
-export const useUpdate = create(
-  devtools<UpdateState & UpdateAction>((set) => ({
-    state: "",
-    setUpdate: (state: string) => set(() => ({ state: state })),
-  })),
-);
+export const useUpdate = create<UpdateState & UpdateAction>((set) => ({
+  state: "",
+  setUpdate: (state: string) => set(() => ({ state: state })),
+}));
