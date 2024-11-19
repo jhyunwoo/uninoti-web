2f2254679a235040d4a5c630dcb79148f49f0d76
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-19 10:30:54 +0900

    fix sitemap and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import Footer from "@/app/components/footer"
 
 export const metadata: Metadata = {
   title: "유니노티",
-  description: "대학교 합격자 발표 알림 서비스. 서울대학교 수시 합격 발표 알림",
+  description:
+    "대학교 합격자 발표 알림 서비스. 서울대, 연세대, 고려대, 중앙대 수시 합격 발표 알림",
   metadataBase: new URL("https://uninoti.moveto.kr"),
 }
 
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,5 +20,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 0.9,
     },
+    {
+      url: "https://uninoti.moveto.kr/yonsei-talent-international-final",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://uninoti.moveto.kr/korea-gaejuck-final",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://uninoti.moveto.kr/cau-tamgu-1st",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
   ]
 }
